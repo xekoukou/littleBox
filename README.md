1063b63589f7454699e8396cf381d64f90cd3a8e
Apostolis Xekoukoulotakis <xekoukou@gmail.com>
2013-04-04 07:46:52 +0300

    moved amber as a submodule

--- README.md
+++
@@ -1,5 +0,0 @@
-littleBox
-==
-
-A component based web development package written in [Amber smalltalk](http://amber-lang.net/).
-=======
--- js/init.js
+++
@@ -1,7 +0,0 @@
-smalltalk.initialize();
-
-/* Similar to jQuery(document).ready() */
-
-if(this.amber && this.amber.smalltalkReady) {
-	this.amber.smalltalkReady();
-}
